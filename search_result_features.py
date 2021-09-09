92d40c07550c1c4c0ec6afdf78722d71f63322e9
capjamesg <jamesg@jamesg.blog>
2021-09-09 08:03:20 +0100

    add headers to search page for indieweb pages, recognise 'what were' in search

--- search_result_features.py
+++ search_result_features.py
@@ -42,9 +42,9 @@ def generate_featured_snippet(cleaned_value, special_result, nlp, url=None, post
     all_locations = []
 
     # if "what is" in original_cleaned_value
-    if "what is" in original_cleaned_value or "what are" in original_cleaned_value or direct == True or "microformats" in original_cleaned_value:
+    if "what is" in original_cleaned_value or "what are" in original_cleaned_value or "what were" in original_cleaned_value or direct == True or "microformats" in original_cleaned_value:
         #  if "is a" in p.text.lower() or "are an" in p.text.lower() and original_cleaned_value.lower() in p.text.lower() 
-        original_cleaned_value = original_cleaned_value.replace("what is", "").replace("what are", "").strip().lower()
+        original_cleaned_value = original_cleaned_value.replace("what is", "").replace("what are", "").replace("what were", "").strip().lower()
 
         p_tags = [p.text for p in soup.find_all(["p", "span"]) if ("p-summary" in p.attrs.get("class", []) and p.text != None) or "{} is".format(original_cleaned_value) in p.text.lower() or "{} are".format(original_cleaned_value) in p.text.lower()]
 
