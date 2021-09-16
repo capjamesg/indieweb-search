6b6bfb955281a89888d6c36dfc27ea39114d2c4c
capjamesg <jamesg@jamesg.blog>
2021-09-16 08:09:18 +0100

    implement concurrency to improve speed, change crawling logic, add logging

--- build_index.py
+++ build_index.py
@@ -10,7 +10,8 @@ import sys
 import os
 from shutil import copyfile
 import config
-import crawler.url_handling as url_handling
+import concurrent_url_handling as url_handling
+import concurrent.futures
 
 start_time = datetime.datetime.now()
 
@@ -25,12 +26,13 @@ def initialize_folders():
 		except:
 			print("{} already created. Skipping creation.".format(d))
 
-	files_to_create = ["static/index_stats.csv", "static/all_links.csv", "static/external_link_status.csv"]
+	files_to_create = ["static/index_stats.csv", "static/all_links.csv", "static/external_link_status.csv", "data/all_links.csv"]
 
 	header_rows_to_write = [
 		"total_pages_indexed,total_images_indexed,pages_indexed_in_last_crawl,images_indexed_in_last_crawl,html_content_length,last_index_start,last_index_end,crawl_duration,external_links_found",
 		"page_linking,link_to,datetime,link_type",
-		"url,status_code,domain_name,date"
+		"url,status_code,domain_name,date",
+		"page_linking,link_to,datetime,link_type",
 	]
 
 	for f in range(0, len(files_to_create)):
@@ -60,9 +62,13 @@ def find_robots_directives(site_url):
 		Finds the robots.txt file on a website, reads the contents, then follows directives.
 	"""
 	try:
-		read_robots = requests.get("https://{}/robots.txt".format(site_url), headers=config.HEADERS)
+		read_robots = requests.get("https://{}/robots.txt".format(site_url), headers=config.HEADERS, timeout=5)
 	except requests.exceptions.RequestException as e:
 		logging.error("Error with site: {}".format(e))
+		with open("broken_domains", "a") as file:
+			file.write("{}".format(site_url))
+		return "broken", []
+	except requests.exceptions.ConnectionError:
 		with open("broken_domains", "a") as file:
 			file.write("{}".format(site_url))
 		return "broken", []
@@ -72,7 +78,7 @@ def find_robots_directives(site_url):
 
 	if read_robots.status_code == 404:
 		logging.warning("Robots.txt not found on {}".format(site_url))
-		url_handling.log_error("https://{}/robots.txt".format(site_url), read_robots.status_code, "Site is missing a robots.txt file.", discovered_urls, broken_urls)
+		# url_handling.log_error("https://{}/robots.txt".format(site_url), read_robots.status_code, "Site is missing a robots.txt file.", discovered_urls, broken_urls)
 
 		return [], ["https://{}/sitemap.xml".format(site_url)]
 
@@ -162,151 +168,188 @@ def log_contents(pages_indexed, images_indexed):
 
 	# Send notification if there are crawl errors
 
-	if len(broken_urls) > 0:
-		message = "{} errors found in crawl.".format(len(broken_urls))
+	# if len(broken_urls) > 0:
+	# 	message = "{} errors found in crawl.".format(len(broken_urls))
 		
 		# requests.post("https://maker.ifttt.com/trigger/crawl_issue/with/key/S", data={"value1": message})
 
 	print("Indexing complete.")
 	logging.debug("Indexing complete.")
 
-def build_index():
+def process_domain(site):
 	# read crawl_queue.txt
-	crawl = True
-	while crawl == True:
-		# read crawl_queue.txt
-		with open("crawl_queue.txt", "r") as file:
-			file = file.readlines()
-			next_site = file[0].strip().replace(", ", ",").split(",")
-			site = next_site[0]
-			if len(next_site) > 1:
-				# let user specify crawl budget for each site
-				if next_site[1].isnumeric():
-					crawl_budget = int(next_site[1])
-				elif next_site[1] == "RECRAWL":
-					crawl_budget = 50
-				else:
-					crawl_budget = 1000
+	# with open("crawl_queue2.txt", "r") as file:
+	# 	file = file.readlines()
+	# 	next_site = file[0].strip().replace(", ", ",").split(",")
+	# 	site = next_site[0]
+	# 	if len(next_site) > 1:
+	# 		# let user specify crawl budget for each site
+	# 		if next_site[1].isnumeric():
+	# 			crawl_budget = int(next_site[1])
+	# 		elif next_site[1] == "RECRAWL":
+	# 			crawl_budget = 50
+	# 		else:
+	# 			crawl_budget = 1000
+	# 	else:
+	# 		crawl_budget = 1000
+			
+	# 	print("Crawl budget: {}".format(str(crawl_budget)))
+
+	final_urls = {}
+	
+	namespaces_to_ignore, sitemap_urls = find_robots_directives(site)
+
+	if namespaces_to_ignore == "broken":
+		return 0, [], site
+
+	# Parse sitemap indexes
+	for u in sitemap_urls:
+		r = requests.get(u)
+		if r.status_code == 200:
+			# parse with bs4
+			soup = BeautifulSoup(r.text, "html.parser")
+			if soup.find("sitemapindex"):
+				# find all the urls
+				all_sitemaps = soup.find_all("loc")
+				for sitemap in all_sitemaps:
+					sitemap_urls.append(sitemap.text)
+					print("will crawl URLs in {} sitemap".format(sitemap.text))
+					logging.debug("will crawl URLs in {} sitemap".format(sitemap.text))
 			else:
-				crawl_budget = 1000
-			print("Crawl budget: {}".format(str(crawl_budget)))
+				print("will crawl URLs in {} sitemap".format(u))
+				logging.debug("will crawl URLs in {} sitemap".format(u))
 
-		if site == "STOP":
-			crawl = False
-		
-		if os.path.isfile("search.db") == True:
-			copyfile("search.db", "new_search.db")
+	sitemap_urls = list(set(sitemap_urls))
 
-		final_urls = {}
+	for s in sitemap_urls:
+		# Only URLs not already discovered will be added by this code
+		# Lastmod dates will be added to the final_url value related to the URL if a lastmod date is available
+		feed = requests.get(s, headers=config.HEADERS)
 
-		# Create new tables if index does not already exist
+		soup = BeautifulSoup(feed.content, "lxml")
 
-		# if not os.path.isfile("search.db"):
-		# 	print("please run 'flask seed build-tables' before attempting to build the index.")
-		# else:
-		# 	# Add URLs already discovered to the indexing queue
-		# 	already_discovered_urls = cursor.execute("SELECT url FROM posts WHERE URL LIKE ? OR url LIKE ?", ("https://{}%".format(config.SITE_URL),"http://{}%".format(config.SITE_URL),)).fetchall()
+		urls = list(soup.find_all("url"))
 
-		# 	if already_discovered_urls:
-		# 		for u in already_discovered_urls:
-		# 			final_urls[u[0]] = ""
-
-		# Delete all old sitemaps and start from scratch
-		# This is key because sometimes sitemaps change and I don't want to retrieve old ones
-		# cursor.execute("DELETE FROM sitemaps;")
-		
-		namespaces_to_ignore, sitemap_urls = find_robots_directives(site)
-
-		if namespaces_to_ignore == "broken":
-			continue
-
-		# Parse sitemap indexes
-		for u in sitemap_urls:
-			r = requests.get(u)
-			if r.status_code == 200:
-				# parse with bs4
-				soup = BeautifulSoup(r.text, "html.parser")
-				if soup.find("sitemapindex"):
-					# find all the urls
-					all_sitemaps = soup.find_all("loc")
-					for sitemap in all_sitemaps:
-						sitemap_urls.append(sitemap.text)
-						# cursor.execute("INSERT INTO sitemaps VALUES (?)", (sitemap.text, ))
-						print("will crawl URLs in {} sitemap".format(sitemap.text))
-				else:
-					# cursor.execute("INSERT INTO sitemaps VALUES (?)", (u, ))
-					print("will crawl URLs in {} sitemap, added sitemap to database".format(u))
-			elif r.status_code == 404:
-				url_handling.log_error(u, r.status_code, "{} sitemap returns a 404 error.".format(u), discovered_urls, broken_urls)
-
-		sitemap_urls = list(set(sitemap_urls))
-
-		for s in sitemap_urls:
-			# Only URLs not already discovered will be added by this code
-			# Lastmod dates will be added to the final_url value related to the URL if a lastmod date is available
-			feed = requests.get(s, headers=config.HEADERS)
-
-			soup = BeautifulSoup(feed.content, "lxml")
-
-			urls = list(soup.find_all("url"))
-
-			for u in urls:
-				if u.find("lastmod"):
-					final_urls[u.find("loc").text] = u.find("lastmod").text
-				else:
-					final_urls[u.find("loc").text] = ""
-
-		image_urls = []
-
-		if len(final_urls) == 0:
-			final_urls["https://{}".format(site)] = ""
-
-		pages_indexed = 0
-		images_indexed = 0
-
-		iterate_list_of_urls = list(final_urls.keys())
-
-		# if reindex argument passed to command
-		# if len(sys.argv) > 1 and sys.argv[1] == "reindex":
-		# 	indexed_already = cursor.execute("SELECT url FROM posts WHERE url LIKE ? or url LIKE ?", ("https://{}%".format(site), "http://{}%".format(site))).fetchall()
-		# 	iterate_list_of_urls = [item for item in iterate_list_of_urls if item not in indexed_already]
-
-		# 	# Make sure home page is part of reindex
-		# 	if site not in iterate_list_of_urls:
-		# 		iterate_list_of_urls.append(site)
-
-		pages_indexed, images_indexed, iterate_list_of_urls, all_links = url_handling.crawl_urls(final_urls, namespaces_to_ignore, pages_indexed, images_indexed, image_urls, links, external_links, discovered_urls, broken_urls, iterate_list_of_urls, site, crawl_budget)
-
-		# log_contents(pages_indexed, images_indexed)
-
-		with open("crawl_queue.txt", "r") as f:
-			rows = f.readlines()[1:]
-
-		with open("crawl_queue.txt", "w+") as f:
-			if len(rows) != 0:
-				for r in rows:
-					f.write(r)
+		for u in urls:
+			if "/tag/" in u.find("loc").text:
+				continue
+			if u.find("lastmod"):
+				final_urls[u.find("loc").text] = u.find("lastmod").text
 			else:
-				with open("domains.txt", "r") as domains:
-					all_domains = domains.readlines()
-					for d in all_domains:
-						f.write(d)
-						f.write("STOP")
+				final_urls[u.find("loc").text] = ""
 
+	return 1000, final_urls, namespaces_to_ignore
 
-		# get date
-		date = datetime.datetime.now().strftime("%Y-%m-%d")
+def build_index(site):
+	# read crawl_queue.txt
+	crawl_budget, final_urls, namespaces_to_ignore = process_domain(site)
 
-		with open("crawled.txt", "a") as f:
-			f.write("{}, {}".format(site, date))
+	if crawl_budget == 0:
+		return
 
-		with open(ROOT_DIRECTORY + "/data/all_links.csv","w") as f:
-			wr = csv.writer(f)
-			wr.writerow(["page_linking", "link_to", "datetime", "link_type", "anchor_text"])
-			wr.writerows(all_links)
+	image_urls = []
 
-		del all_links
+	if len(final_urls) == 0:
+		final_urls["https://{}".format(site)] = ""
 
-		#broken_links.check_for_invalid_links()
+	images_indexed = 0
 
-build_index()
+	iterate_list_of_urls = list(final_urls.keys())
+
+	try:
+		verify_url = requests.get(iterate_list_of_urls[0], headers=config.HEADERS, timeout=5)
+	except:
+		print("error with {} url and site, skipping".format(iterate_list_of_urls[0]))
+		logging.debug("error with {} url and site, skipping".format(iterate_list_of_urls[0]))
+		return
+
+	print("processing {}".format(site))
+	logging.debug("processing {}".format(site))
+
+	# if reindex argument passed to command
+	# if len(sys.argv) > 1 and sys.argv[1] == "reindex":
+	# 	indexed_already = cursor.execute("SELECT url FROM posts WHERE url LIKE ? or url LIKE ?", ("https://{}%".format(site), "http://{}%".format(site))).fetchall()
+	# 	iterate_list_of_urls = [item for item in iterate_list_of_urls if item not in indexed_already]
+
+	# 	# Make sure home page is part of reindex
+	# 	if site not in iterate_list_of_urls:
+	# 		iterate_list_of_urls.append(site)
+
+	indexed = 0
+	valid_count = 0	
+
+	indexed_list = {}
+
+	all_links = []
+	
+	for url in iterate_list_of_urls:
+		url_indexed, discovered, valid, new_links = url_handling.crawl_urls(final_urls, namespaces_to_ignore, indexed, images_indexed, image_urls, links, external_links, discovered_urls, broken_urls, iterate_list_of_urls, site, crawl_budget, url)
+
+		if valid == True:
+			valid_count += 1
+
+		all_links.append([[url_indexed, link] for link in new_links])
+
+		if valid_count == 0 and indexed == 25:
+			break
+		
+		indexed += 1
+
+		print("{}/{}".format(indexed, crawl_budget))
+		logging.debug("{} - {}/{}".format(site, indexed, crawl_budget))
+
+		if indexed > crawl_budget:
+			break
+
+		indexed_list[url_indexed] = True
+
+		for item in discovered.keys():
+			if not indexed_list.get(item):
+				print("{} not indexed, added".format(item))
+				iterate_list_of_urls.append(item)
+
+	with open("crawl_queue2.txt", "r") as f:
+		rows = f.readlines()
+
+	rows.remove(site + "\n")
+
+	with open("crawl_queue2.txt", "w+") as f:
+		f.writelines(rows)
+
+	# get date
+	date = datetime.datetime.now().strftime("%Y-%m-%d")
+
+	with open("crawled2.txt", "a+") as f:
+		f.write("{}, {}".format(site, date))
+
+	with open(ROOT_DIRECTORY + "/data/all_links.csv","w") as f:
+		wr = csv.writer(f)
+		wr.writerow(["page_linking", "link_to"])
+		wr.writerows(all_links)
+
+	# broken_links.check_for_invalid_links()
+
+with open("crawl_queue2.txt", "r") as f:
+	rows = f.readlines()
+
+futures = []
+
+sites_indexed = 0
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=15) as executor:
+	futures = [executor.submit(build_index, url.replace("\n", "")) for url in rows]
+	
+	while len(futures) > 0:
+		for future in concurrent.futures.as_completed(futures):
+			sites_indexed += 1
+
+			print("SITES INDEXED: {}".format(sites_indexed))
+			logging.info("SITES INDEXED: {}".format(sites_indexed))
+
+			try:
+				url_indexed, discovered = future.result()
+
+				futures.remove(future)
+			except Exception as e:
+				print(e)
+				logging.error(e)
