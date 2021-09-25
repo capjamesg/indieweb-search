fddbd0751d52d14b809d01442ace218fe2426720
capjamesg <jamesg@jamesg.blog>
2021-09-25 12:27:37 +0100

    remove old files

--- models.py
+++
@@ -1,8 +0,0 @@
-from . import db
-from flask_login import UserMixin
-
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
-    username = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
-    api_key = db.Column(db.String(100))
